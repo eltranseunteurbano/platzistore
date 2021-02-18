4250b31cec0311eab2d64a83e15987bd8c09b8f3
Jaime Burbano <burbanojaime98@gmail.com>
2021-02-17 22:03:30 -0500

    integración de react context

---
+++ src/hooks/useInitialState.js
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import initialState from '../initialState';
+
+const useInitialState = () => {
+  const [state, setState] = useState();
+  initialState;
+
+  const addToCart = (payload) => {
+    setState({
+      ...state,
+      cart: [...state.cart, payload],
+    });
+  };
+
+  const removeFromCart = (payload) => {
+    setState({
+      ...state,
+      cart: state.cart.filter((item) => item.id !== payload.id),
+    });
+  };
+
+  return {
+    //functions
+    addToCart,
+    removeFromCart,
+    //states
+    state,
+  };
+};
+
+export default useInitialState;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import AppProvider from '../context/AppContext';
 import Home from '../containers/Home';
 import Checkout from '../containers/Checkout';
 import Information from '../containers/Information';
@@ -9,18 +10,20 @@ import NotFound from '../containers/NotFound';
 import Layout from '../components/Layout';
 
 const App = () => (
-  <BrowserRouter>
-    <Layout>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/checkout" component={Checkout} />
-        <Route exact path="/checkout/information" component={Information} />
-        <Route exact path="/checkout/payment" component={Payment} />
-        <Route exact path="/checkout/success" component={Success} />
-        <Route path="/404" component={NotFound} />
-      </Switch>
-    </Layout>
-  </BrowserRouter>
+  <AppProvider>
+    <BrowserRouter>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/checkout" component={Checkout} />
+          <Route exact path="/checkout/information" component={Information} />
+          <Route exact path="/checkout/payment" component={Payment} />
+          <Route exact path="/checkout/success" component={Success} />
+          <Route path="/404" component={NotFound} />
+        </Switch>
+      </Layout>
+    </BrowserRouter>
+  </AppProvider>
 );
 
 export default App;
